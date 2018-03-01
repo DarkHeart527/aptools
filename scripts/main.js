d0014f976c0d456207eb21d719ff89d156a8e026
James <DarkHeart527@users.noreply.github.com>
2018-03-01 08:49:12 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-
+const at = {
+  getTime: function(type) {
+    if (type == 'normal') {
+      let time = new Date();
+      let hour = time.getHours();
+      if (hour > 12) {
+        hour = Number(hour) - 12;
+      }
+      let minute = time.getMinutes();
+      time = `${hour}:${minute}`;
+      return time;
+    } else {
+      at.getTime('normal');
+    }
+  },
+  initialLeapYear: 2000,
+}
