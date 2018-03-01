8f969baf7cc599efb7e0eab9f560aae626307b1a
James <DarkHeart527@users.noreply.github.com>
2018-03-01 11:09:11 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,12 +13,12 @@ const at = {
     else if (type == 'extended') {
       let time = new Date();
       let hour = time.getHours();
-      //let second = time.getSeconds();
+      let second = time.getSeconds();
       if (hour > 12) {
         hour = Number(hour) - 12;
       }
       let minute = time.getMinutes();
-      time = `${hour}:${minute}:`;
+      time = `${hour}:${minute}:${second}`;
       return time;
     } else {
       at.getTime('normal');
